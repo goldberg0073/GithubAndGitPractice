5dd19d334fbcbf1e3c81497155c82e089caba160
goldberg0073 <55060367+goldberg0073@users.noreply.github.com>
2020-11-28 09:43:46 -0500

    Update GitTests.java

--- src/test/java/com/gitTests/GitTests.java
+++ src/test/java/com/gitTests/GitTests.java
@@ -6,7 +6,15 @@ public class GitTests {
     public static void main(String[] args) {
 
         System.out.println("Before creating repo");
+        
+        System.out.println("devops made some changes "4" ==> needs to be fetch to my branche main local ");
+        System.out.println("1");
+        System.out.println("2");
+        System.out.println("3");
+        System.out.println("4");
 
 
+        
+        
     }
 }
